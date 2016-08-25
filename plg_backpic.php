26208b13b52e1fcb6b137d7a3dfaabe287a3a96a
shahryarjb <shahryar@shahryars-MacBook-Air.local>
2016-08-26 01:29:29 +0430

    version -> 0.0.2

--- plg_backpic.php
+++ plg_backpic.php
@@ -2,26 +2,26 @@
 /**
  * @package     Joomla.Plugin
  * @subpackage  Content.Contact
- *
- * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @version		0.2
+ * @copyright   Copyright (C) 2016 - 2016 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * &@license    https://trangell.com
  */
 
 defined('_JEXEC') or die;
 
-use Joomla\Registry\Registry;
-
-class PlgContentBackpic extends JPlugin {
+//use Joomla\Registry\Registry;
+class PlgContentPlg_Backpic extends JPlugin {
 	protected $db;
+	protected $autoloadLanguage = true;
 
 	public function onContentPrepare() {
 		if (JRequest::getVar('option')==='com_content' && JRequest::getVar('view')==='article') {
 			$articleId = JRequest::getInt('id');
 			//$db =& JFactory::getDBO();
 		}else {
-			return false;
+			return false;	
 		}
-		
 			$db    = JFactory::getDbo();
 			$query = $db->getQuery(true);
 			
@@ -29,21 +29,48 @@ class PlgContentBackpic extends JPlugin {
 			$query->select('*');
 			$query->from('#__backpic');
 			$query->where('article_id = ' . $articleId);
+			$query->where('published = 1');
+			$query->where('type = 1');
 				 
 			$db->setQuery($query);
 			$row = $db->loadAssoc();
-			// echo "</br> image : " . $row['pic'];
-			$row['pic'] = htmlspecialchars($row['pic']);
-			$row['width'] = htmlspecialchars($row['width']);
-			$row['height'] = htmlspecialchars($row['height']);
+
+			if ($row['published'] == 1 AND $row['type'] == 1) {
+				
+				$row['pic'] 			= htmlspecialchars($row['pic'], ENT_QUOTES, 'UTF-8');
+				$row['width'] 			= htmlspecialchars($row['width'], ENT_QUOTES, 'UTF-8');
+				$row['height'] 			= htmlspecialchars($row['height'], ENT_QUOTES, 'UTF-8');
+				$row['custom'] 			= htmlspecialchars($row['custom'], ENT_QUOTES, 'UTF-8');
+				$row['menudbid']		= htmlspecialchars($row['menudbid'], ENT_QUOTES, 'UTF-8');
+				$row['template_name']	= htmlspecialchars($row['template_name'], ENT_QUOTES, 'UTF-8');
+
+				//echo $row['template_name'];
+
+				if (!empty($row['template_name']) AND !empty($row['template_name'] == 0)) {
+
+					$selectsetTemplate = JFactory::getApplication();
+			 		$selectsetTemplate->setTemplate("{$row['template_name']}", null);
+				}
 			
 			$doc = JFactory::getDocument();
 			$UrlSite = JURI::root();
-			$doc->addStyleDeclaration("
-			body {background: url('{$UrlSite}{$row['pic']}') no-repeat;background-size: {$row['width']} {$row['height']};}
-			");
-		
-	}
-	
+				if (!empty($row['pic'])) {
+					$doc->addStyleDeclaration("
+						body {background: url('{$UrlSite}{$row['pic']}') no-repeat ;background-size: {$row['width']} {$row['height']};}
+				");
+				}
+				
+				if (!empty($row['custom'])) {
+					$doc->addStyleDeclaration("
+						{$row['custom']}
+				");
+				}
+			}
+			
+
+	}	
 
 }
+
+
+
